30355ae84f64089d770766d54d1987ea42621845
kareem_m0hmed <kareemmhammed53@gmail.com>
2026-06-12 21:39:08 +0300

    feat: add search bar in home screen

--- app/src/main/java/com/example/kotline_commerceapplication/Presentation/Screens/HomeScreen.kt
+++ app/src/main/java/com/example/kotline_commerceapplication/Presentation/Screens/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.example.kotline_commerceapplication.Presentation.Screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.kotline_commerceapplication.Core.Extensions.H
 import com.example.kotline_commerceapplication.Presentation.component.CustomHomTopBarComponent
+import com.example.kotline_commerceapplication.Presentation.component.CustomTextFormFiled
 
 @Composable
 
@@ -16,13 +19,21 @@ fun HomeScreen() {
         modifier = Modifier.padding(vertical = 20.dp, horizontal = 16.dp),
         topBar = {
             CustomHomTopBarComponent(
-                "Kareem"
+                "Kareem",
+                notificationsCount = 4
             )
         },
         content = { paddingValues ->
-            Box(
-                modifier = Modifier.padding(paddingValues)
-            )
+            Column(modifier = Modifier.padding(paddingValues)) {
+                24.H
+                CustomTextFormFiled(
+                    label = {
+                        Text("Search")
+                    },
+                    value = "",
+                    onValueChange = {}
+                )
+            }
         }
     )
 }
